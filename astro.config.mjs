572943188923f42237b8e5f3ba8fef1a18bde67c
Nemanja <nstojanovic@itbts.net>
2024-01-17 19:51:23 +0100

    chore: remove astro-image-tools from config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import { astroImageTools } from "astro-imagetools";
 import { defineConfig } from "astro/config";
 import config from "./src/config/config.json";
 
@@ -20,7 +19,6 @@ export default defineConfig({
       },
     }),
     mdx(),
-    astroImageTools,
   ],
   markdown: {
     remarkPlugins: [],
