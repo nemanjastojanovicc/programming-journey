0a4a4939f49838ac78d9547a2f38d623138d868d
Nemanja <nstojanovic@itbts.net>
2024-01-22 00:48:23 +0100

    style: use dark-plus for markdown shiki config theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,7 +24,7 @@ export default defineConfig({
   markdown: {
     remarkPlugins: [],
     shikiConfig: {
-      theme: "one-dark-pro",
+      theme: "dark-plus",
       wrap: true,
     },
     extendDefaultPlugins: true,
